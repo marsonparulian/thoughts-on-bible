97afa2a6ec17984b8f2fbf7a2792948e447e3833
marson parulian <marson.parulian@gmail.com>
2025-02-13 12:55:25 +1100

    Timeline: Init project

---
+++ timeline/README.md
@@ -0,0 +1,16 @@
+# Bible Timeline
+
+12 Feb 2025
+ 
+## Objective
+        Introduce timeline in the bible, starting from the most significant characters / events to more detailed events. 
+
+## How
+The timeline will have series of `node`, in which every node contains 1 or more keywords (character / event). 
+
+At start, there will be few `node`. User can increase the `step` which will introduce 1 focus `node` in the timeline. After the `step` is increased, the `node.focused` will be switched to new `node`.
+
+If the page is refreshed, the `step` must be persisted. So the `step` need to be persisted in the URL.
+
+## Future Enhancement
+There will be 1 or  more focused `node` which will replace `node` to give more detailed info. For instance: a "Moses, 10 Commandments", in certain step, could be replaced with "Moses, 10 Plagues", "Crossing the Red Sea", "10 Commandments", and "40 years in Wilderness" nodes.
---
+++ timeline/timeline.js
@@ -0,0 +1,51 @@
+
+
+$(document).ready(function () {
+    $(document).foundation();
+
+    // Set `step` from the searchParam or set to 0
+    let url = new URL(window.location.href);
+    let step = url.searchParams.get('step');
+    step ??= 0;
+
+    // Setup `step` related data
+    const stepSeries = constructStepSeries();
+
+    // console.log(`step : ${step}`);
+
+    /**
+     * Construct list of `step`. Each `step` contains array of `node`.
+     * The `node` will be ordered from the least value of `data-step` attribute. Nodes with the same `data-step` value will be grouped in the same `step`.
+     */
+    function constructStepSeries() {
+        // Collect all values of `data-step`
+        let steps = Array.from(document.querySelectorAll('[data-step]')).map(node => node.getAttribute('data-step'));
+        console.log(steps);
+
+        // Remove duplicate values
+
+        // Order the `data-step` values ascending
+
+        // Query all `node`s with the same `data-step` value and group them in the same `step`
+    }
+    /**
+     * Decide what is the next step
+     */
+    function nextStep() {
+        // TODO Decide what is the next step based on the current step value
+        let nextStep = 999;
+
+        set(nextStep)
+    }
+    /**
+     * Set the current step 
+     */
+    function setStep(step) {
+        // TODO Show all `node` elements which should be shown based on the step value
+
+
+        // Set the current step in the URL
+        url.searchParams.set('step', step);
+        window.history.replaceState({}, '', url);
+    }
+});
